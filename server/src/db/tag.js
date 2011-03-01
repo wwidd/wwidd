c262ab596b15699c13fcb5533be5ae39a3077cee
Dan Stocker <dan@kwaia.com>
2011-03-01 22:25:04 +0100

    no temp table for single tag update

--- server/src/db/tag.js
+++ server/src/db/tag.js
@@ -12,18 +12,27 @@ tag = function () {
 	self.set = function (before, after, handler) {
 		var	where =  clause(before).join(" AND "),
 		
-		statement = [
+		statement = (before.mediaid ? [
+			// updaing a single tag
 			"BEGIN TRANSACTION",
-			// marking mediaids that contain the old tag
+			// deleting old tag name
+			["DELETE FROM", self.kind, "WHERE", where].join(" "),
+			// inserting new name
+			["INSERT OR IGNORE INTO", self.kind, "(mediaid, tag)", "VALUES", "(" + [before.mediaid, "'" + after.tag + "'"].join(",") + ")"].join(" "),
+			"COMMIT"
+		] : [
+			// updaing multiple tags
+			"BEGIN TRANSACTION",
+			// marking mediaids that contain the old tag name
 			"CREATE TEMPORARY TABLE focus (mediaid INTEGER)",
 			["INSERT INTO focus (mediaid) SELECT mediaid FROM", self.kind, "WHERE", where].join(" "),
 			// deleting entries that already contain both the old and new names
 			["DELETE FROM", self.kind, "WHERE mediaid IN focus AND", where].join(" "),
-			// inserting new values
+			// inserting new tag names
 			["INSERT OR IGNORE INTO", self.kind, "(mediaid, tag)", "SELECT mediaid, '" + after.tag + "' AS tag FROM focus"].join(" "),
 			"DROP TABLE focus",
 			"COMMIT"
-		].join(";\n");
+		]).join(";\n");
 		
 		console.log(statement);
 		
