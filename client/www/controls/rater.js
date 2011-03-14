81da675c66b3c02e75fece7998b022a9c0e7fc41
Dan Stocker <dan@kwaia.com>
2011-03-14 16:21:09 +0100

    rendering improvements

--- client/www/controls/rater.js
+++ client/www/controls/rater.js
@@ -4,36 +4,36 @@
 // The number of stars represent the rating. Up to five stars can be given.
 ////////////////////////////////////////////////////////////////////////////////
 var controls = function (controls, $, services) {
+	// generates a rater UI with no. of stars equal to rating
+	function getUI(row) {
+		// constructing jQuery object
+		var $result = $('<div />', {'class': 'rater'}),
+				i;
+		for (i = 0; i < 5; i++) {
+			$result.append('<a href="#" />');
+		}
+
+		// filling stars
+		if (row.rating > 0) {
+			$result.find('a')
+				.eq(row.rating - 1)
+					.prevAll()
+					.andSelf()
+						.addClass('star');
+		}
+		
+		return $result;
+	}
+	
 	controls.rater = function (row) {
 		var base = controls.control,
 				self = Object.create(base);
 
-		// generates a rater UI with no. of stars equal to rating
-		function getUI(rating) {
-			// constructing jQuery object
-			var $result = $('<div />', {'class': 'rater'}),
-					i;
-			for (i = 0; i < 5; i++) {
-				$result.append('<a href="#" />');
-			}
-
-			// filling stars
-			if (row.rating > 0) {
-				$result.find('a')
-					.eq(row.rating - 1)
-						.prevAll()
-						.andSelf()
-							.addClass('star');
-			}
-			
-			return $result;
-		}
-		
 		self.getUI = function () {
 			var 
 			
 			// creates a UI with mouse handler
-			$result = getUI(row.rating)
+			$result = getUI(row)
 				.mouseleave(function () {
 					self.redraw();	
 				}),
