0496ed74d37a8cc7ddbff2ce3d25e6686068a13f
Dan Stocker <dan@kwaia.com>
2011-03-16 23:38:02 +0100

    kind events are added using .live()

--- client/www/controls/kind.js
+++ client/www/controls/kind.js
@@ -9,19 +9,25 @@ var controls = function (controls, $, data) {
 		
 		var self = Object.create(controls.base());
 		
+		self.data.row = row;
+		
 		//////////////////////////////
 		// Event handlers
 
 		// checkbox event handler
 		// when checkbox or label is clicked
 		function onChecked() {
-			var checked = $(this).attr('checked');
+			var	$this = $(this),
+					row = controls.lookup[$this.parent().attr('id')].data.row,
+					checked = $this.attr('checked');
 			handler(row.kind, checked);
 		}
 		// when tag background is clicked
 		function onClick(event) {
 			if (event.target === this) {
-				var $checkbox = $(this).find(':checkbox'),
+				var $this = $(this),
+						row = controls.lookup[$this.attr('id')].data.row,
+						$checkbox = $this.find(':checkbox'),
 						checked = !$checkbox.attr('checked');
 				$checkbox.attr('checked', checked);
 				handler(row.kind, checked);
@@ -32,15 +38,15 @@ var controls = function (controls, $, data) {
 		// Overrides
 
 		self.init = function (elem) {
-			elem
-				.click(onClick)
-				.find(':checkbox').click(onChecked).end();
+			$('.kind').live('click', onClick);
+			$('.kind :checkbox').live('click', onChecked);
+			return false;
 		};
 		
 		self.html = function () {
 			var id = 'kind' + row.kind;
 			return [
-				'<span id="', self.id, '" class="tag display ', data.kinds.getNumber(row.kind), '">',
+				'<span id="', self.id, '" class="kind tag display ', data.kinds.getNumber(row.kind), '">',
 				'<input type="checkbox" id="', id, '"', !self.parent || !self.parent.hidden(row.kind) ? ' checked="checked"' : '', '/>',
 				'<label for="', id, '">', row.kind ? row.kind : "[default]", '</label>',
 				'</span>'
