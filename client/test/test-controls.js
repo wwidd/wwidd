b05a668a45d59ded4e7fb883333413fbaa608e54
Dan Stocker <dan@kwaia.com>
2011-03-17 00:45:31 +0100

    fixed control unit tests

--- client/test/test-controls.js
+++ client/test/test-controls.js
@@ -6,62 +6,62 @@ var test = function (test, $, controls) {
 		module("Controls");
 		
 		////////////////////////////////////////////////////////////////////////////////
-
-		test("[control] Exceptions", function () {
-			var test_control = Object.create(controls.control);
-				
-			raises(test_control.getUI, null, "getUI() of base class throws mandatory exception");
-			raises(test_control.redraw, null, "Unappended controls can't be redrawn");
-		});
 		
 		test("[control] Appending", function () {
-			var test_control = Object.create(controls.control),
+			var test_control = Object.create(controls.base()),
 					$parent = $('<div />'),
-					$child;
+					$child, $ui;
 
-			test_control.getUI = function () {
-				return $('<div />');
+			test_control.html = function () {
+				return '<div />';
 			};
 			
-			equals(test_control.appendTo($parent), $parent, "appendTo() returns the jQuery parent");
-			
-			$child = test_control.UI;
-			test_control.redraw();
-			notEqual(test_control.UI, $child, "Control UI changes after redraw()");
+			equals(test_control.render($parent), $parent, "render() returns the jQuery parent");
+
+			$child = $parent.children()[0];
+			$ui = test_control.render()[0];
+			notEqual($ui, $child, "Control UI changes after render()");
 		});
 		
 		////////////////////////////////////////////////////////////////////////////////
 
 		test("[editable] Exceptions", function () {
-			var test_editable = Object.create(controls.editable);
+			var test_editable = Object.create(controls.editable());
 				
 			raises(test_editable.edit, null, "edit() of base class throws mandatory exception");
 			raises(function () {
-				test_editable.appendTo($('<div />'));
-			}, null, "Can't append control before overriding display()");
+				test_editable.render($('<div />'));
+			}, null, "Can't render control before overriding display()");
 		});
 		
 		test("[editable] Behavior", function () {
-			var test_editable = Object.create(controls.editable),
+			var base_editable = controls.editable(),
+					test_editable = Object.create(base_editable),
 					mode_before = test_editable.mode,
-					$control = $('<div />');
+					$control;
 
 			// mock control
+			test_editable.init = function (elem) {
+				return base_editable.init(test_editable, elem);
+			};
 			test_editable.display = function () {
-				return controls.editable.display(test_editable, $control);
+				return '<span id="' + test_editable.id + '">A</span>';
 			};
 			test_editable.edit = function () {
-				return $('<div />');
+				return '<input id="' + test_editable.id + '" type="text" />';
 			};
-			test_editable.appendTo($('<div />'));
+			test_editable.render($('<div />'));
 			
+			// test 1.
 			test_editable.toggle();
 			notEqual(test_editable.mode, mode_before, "Editable mode changes after toggle()");
 
-			test_editable.toggle('display');			
+			// test 2.
+			$control = test_editable.toggle('display');
 			$control.click();
 			notEqual(test_editable.mode, mode_before, "Clicking control in display mode changes mode to edit");
 			
+			// test 3.
 			$('body').click();
 			equals(test_editable.mode, mode_before, "Clicking outside of edit changes mode back to display");			
 		});
@@ -70,34 +70,37 @@ var test = function (test, $, controls) {
 		// controls.kind is tricky, because we need to test if clicking on the:
 		// checkbox, label and anywhere else on the control triggers the same event
 		
-		test("[kind] Clicking paprts of the control", function () {
+		test("[kind] Clicking parts of the control", function () {
 			var test_kind = controls.kind({kind: "test"}, function () {
 				ok(true, "Click handler triggered");
-			});
-			
-			test_kind.appendTo($('<div />'));
-			
+			}),
+						
+			$parent = test_kind.render($('<div />'));
+
 			expect(2);
-			test_kind.UI.click();
-			test_kind.UI.find(':checkbox').click();
+			$parent.children()
+				.click()
+				.find(':checkbox').click();
 		});
 
 		test("[kind] Checked state", function () {
-			var test_kind = controls.kind({kind: "test"}),
-					before_checked,
-					after_checked;
+			var before_checked,
+					after_checked,
+					
+			test_kind = controls.kind({kind: "test"}),
 			
-			test_kind.appendTo($('<div />'));
-			before_checked = test_kind.UI.find(':checkbox').is(':checked');
-			test_kind.UI.click();
-			after_checked = test_kind.UI.find(':checkbox').is(':checked');
+			$parent = test_kind.render($('<div />'));
+
+			before_checked = $parent.find(':checkbox').is(':checked');
+			$parent.children().click();
+			after_checked = $parent.find(':checkbox').is(':checked');
 			
 			notEqual(after_checked, before_checked, "Checked state changes after clicking the control");
 			before_checked = after_checked;
-			test_kind.UI.click();
-			after_checked = test_kind.UI.find(':checkbox').is(':checked');
+			$parent.children().click();
+			after_checked = $parent.find(':checkbox').is(':checked');
 
-			notEqual(after_checked, before_checked, "Checked state changes back after clicking again");			
+			notEqual(after_checked, before_checked, "Checked state changes back after clicking again");
 		});
 	};
 	
