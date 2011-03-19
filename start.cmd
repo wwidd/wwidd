c2af9593d33dc80dc936f691bec94da4a9398058
Dan Stocker <dan@kwaia.com>
2011-03-19 22:58:20 +0100

    environment variables improved for start.cmd

--- start.cmd
+++ start.cmd
@@ -1,9 +1,6 @@
-REM @ECHO OFF
-
-set env = %CD%\redist\node\env.exe
-%env% PATH=%PATH%;%CD%\redist\node;%CD%\redist\sqlite;%CD%\redist\libextractor;
-
 start http://127.0.0.1:8124
 
+set	CYGWIN=nodosfilewarning
+path %CD%\redist\node;%CD%\redist\sqlite;%CD%\redist\libextractor;%PROGRAMFILES%\VideoLAN\VLC;
 node.exe server/src/server.js
 
