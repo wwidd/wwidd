87f39f907cfea79b712b1dbfa8e3da717970d926
Dan Stocker <dan@kwaia.com>
2011-03-19 20:32:42 +0100

    libextractor added to terminal path

--- start.cmd
+++ start.cmd
@@ -1,7 +1,7 @@
 REM @ECHO OFF
 
-set env = %CD%\redist\node\bin\env.exe
-%env% PATH=%PATH%;%CD%\redist\node\bin;%CD%\redist\sqlite;
+set env = %CD%\redist\node\env.exe
+%env% PATH=%PATH%;%CD%\redist\node;%CD%\redist\sqlite;%CD%\redist\libextractor;
 
 start http://127.0.0.1:8124
 
