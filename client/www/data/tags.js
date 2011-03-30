a9bc54e76749b65e4f229d9744fc476bf7a36a01
Dan Stocker <dan@kwaia.com>
2011-03-31 00:55:27 +0200

    tag lookup improved

--- client/www/data/tags.js
+++ client/www/data/tags.js
@@ -11,6 +11,11 @@ yalp.data = function (data, jOrder, services) {
 			// initializes data object: calls service, populates table
 			init: function () {
 				services.gettags(function (json) {
+					var i, row;
+					for (i = 0; i < json.data.length; i++) {
+						row = json.data[i];
+						row.tag = row.name + '\t' + row.kind;
+					}
 					self.table = jOrder(json.data)
 						.index('tag', ['tag'], {ordered: true, type: jOrder.string})
 						.index('name', ['name'], {ordered: true, grouped: true, type: jOrder.string});
@@ -20,7 +25,7 @@ yalp.data = function (data, jOrder, services) {
 
 			// retrieves the first matching tag to a search term
 			searchTag: function (term) {
-				return (self.table.where([{tag: term}], {mode: jOrder.startof, renumber: true})[0] || {tag: ""}).tag;
+				return (self.table.where([{tag: term}], {mode: jOrder.startof, renumber: true})[0] || {tag: ""}).tag.replace('\t', ':');
 			},
 			
 			searchName: function (term) {
