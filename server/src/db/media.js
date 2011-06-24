05d2a065ce692d27d5bd070cc924ca0868fd5f12
Dan Stocker <dan@kwaia.com>
2011-06-24 09:21:10 +0200

    more accurate search query

--- server/src/db/media.js
+++ server/src/db/media.js
@@ -11,14 +11,14 @@ filter = function (tags, inclusive) {
 			clause = [],
 			i;
 	for (i = 0; i < names.length; i++) {
-		clause.push("name LIKE '" + names[i] + "%'");
+		clause.push("',' || group_concat(name) LIKE '%," + names[i] + "%'");
 	}
 	return [
-		"WHERE mediaid IN (",		
-		"SELECT mediaid FROM tags WHERE",
-		clause.join(" OR "),
-		"GROUP BY mediaid HAVING count(name) >=",
-		inclusive ? 1 : names.length,
+		"WHERE mediaid IN (",
+		"SELECT mediaid FROM tags",
+		"GROUP BY mediaid",
+		"HAVING",
+		clause.join(" AND "),
 		")"
 	].join(" ");
 },
