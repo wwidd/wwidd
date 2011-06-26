4ac0e665cd5e8b4f74809da666cf8f2549025228
Dan Stocker <dan@kwaia.com>
2011-06-26 21:38:29 +0200

    comment

--- client/www/controls/library.js
+++ client/www/controls/library.js
@@ -58,6 +58,7 @@ yalp.controls = (function (controls, $, data) {
 			data.media.init(controls.search.filter, function () {
 				// setting active library in page title
 				$document.attr('title', title + ' - ' + controls.switcher.selected);
+				// resetting preview popup box
 				controls.preview
 					.remove()
 					.render();
