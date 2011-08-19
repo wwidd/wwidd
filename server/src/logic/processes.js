485aefe5ba9094dacf9ff39d2b61a068fbb6708c
Dan Stocker <dan@kwaia.com>
2011-08-19 19:37:12 +0200

    refactoring: redundant object creation eliminated from extractor process chain

--- server/src/logic/processes.js
+++ server/src/logic/processes.js
@@ -13,41 +13,46 @@ processes = {
 	// and relative part are separated by colon
 	// - path: e.g. '/media/HDD|/videos'
 	// - finish: callback for each element in process chain 
-	extractor: chain(function (path, finish) {
-		var tmp = path.split('|'),
-				root = tmp[0],
-				relative = tmp[1];
+	extractor: function () {
+		var
+		
+		lookup = {
+			'duration': 'Duration',
+			'dimensions': 'dimensions',
+			'distributor': 'WM/ContentDistributor',
+			'copyright': 'copyright',
+			'bitrate': 'bitrate',
+			'video codec': 'video codec',
+			'audio codec': 'audio codec',
+			'genre': 'WM/Genre',
+			'adult': 'WM/ParentalRating'
+		};
 
-		// extracting metadata from file w/ ffmpeg
-		ffmpeg.metadata(root + relative, function (data) {
-			// filtering out useful metadata
-			var keywords = {},
-					key, value,
-					
-			lookup = {
-				'duration': 'Duration',
-				'dimensions': 'dimensions',
-				'distributor': 'WM/ContentDistributor',
-				'copyright': 'copyright',
-				'bitrate': 'bitrate',
-				'video codec': 'video codec',
-				'audio codec': 'audio codec',
-				'genre': 'WM/Genre',
-				'adult': 'WM/ParentalRating'
-			};
-			
-			for (key in lookup) {
-				if (lookup.hasOwnProperty(key)) {
-					value = data[lookup[key]];
-					if (typeof value !== 'undefined' && value.length) {
-						keywords[key] = data[lookup[key]];
+		// handler run on each link in process chain
+		return chain(function (path, finish) {
+			var tmp = path.split('|'),
+					root = tmp[0],
+					relative = tmp[1];
+	
+			// extracting metadata from file w/ ffmpeg
+			ffmpeg.metadata(root + relative, function (data) {
+				// filtering out useful metadata
+				var keywords = {},
+						key, value;
+
+				for (key in lookup) {
+					if (lookup.hasOwnProperty(key)) {
+						value = data[lookup[key]];
+						if (typeof value !== 'undefined' && value.length) {
+							keywords[key] = data[lookup[key]];
+						}
 					}
 				}
-			}
-			
-			finish({path: relative, keywords: keywords});
+				
+				finish({path: relative, keywords: keywords});
+			});
 		});
-	})
+	}()
 };
 
 exports.processes = processes;
