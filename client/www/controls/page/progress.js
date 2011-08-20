84ed47bcd3e1d3ffec07efc732deae78c79c31de
Dan Stocker <dan@kwaia.com>
2011-08-20 16:47:02 +0200

    progress indicator control

---
+++ client/www/controls/page/progress.js
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////////////////////////
+// General Progress Indicator Control
+////////////////////////////////////////////////////////////////////////////////
+/*global jQuery */
+var yalp = yalp || {};
+
+yalp.controls = function (controls, $) {
+	controls.progress = function () {
+		var self = controls.control.create(),
+				progress = 0;
+
+		//////////////////////////////
+		// Getters / setters
+
+		// sets the progress bar to a given value
+		// - progress: normalized progress value between 0 and 1 (float)
+		self.progress = function (value) {
+			progress = value;
+			return self;
+		};
+		
+		self.reset = function () {
+			progress = -1;
+			return self;
+		};
+		
+		//////////////////////////////
+		// Overrides
+
+		self.init = function (elem) {
+			if (progress > 1 || progress < 0) {
+				elem.find('.bar')
+					.hide();
+			} else {
+				elem.find('.bar')
+					.show()
+					.css({width: Math.round(progress * 100) + "%"});
+			}
+		};
+
+		self.html = function () {
+			return [
+				'<div id="', self.id, '" class="progress">',
+				'<div class="bar"></div>',
+				'</div>'
+			].join('');
+		};
+		
+		return self;
+	};
+	
+	return controls;
+}(yalp.controls || {},
+	jQuery);
+
