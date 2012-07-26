8fd2937a60795f61a7c5619071330e39185aea53
Dan Stocker <dan@kwaia.com>
2012-07-26 23:47:54 +0200

    superfluous Object.create polyfill removed

--- client/www/js/base/create.js
+++ client/www/js/base/create.js
@@ -1,15 +1,3 @@
-/**
- * Object.create
- */
-if (typeof Object.create !== 'function') {
-    Object.create = function (o) {
-        function F() {}
-
-        F.prototype = o;
-        return new F();
-    };
-}
-
 if (typeof Object.isEmpty !== 'function') {
     Object.isEmpty = function (o) {
         var key;
